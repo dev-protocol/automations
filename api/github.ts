6125746b3a5a83f5c67e3714e8c9e1a5a9f8a905
aggre <hiroyuki.aggre@gmail.com>
2022-03-09 19:05:47 +0900

    change logging terget

--- api/github.ts
+++ api/github.ts
@@ -5,6 +5,6 @@ export default function handler(
 	res: VercelResponse
 ): VercelResponse {
 	// eslint-disable-next-line functional/no-expression-statement
-	console.log({ req, res })
+	console.log({ 'req.body': req.body })
 	return res.status(200).json(req.body)
 }
