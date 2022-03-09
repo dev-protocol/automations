40f720704e5ad3e8bff2d2c217f6eb9780bec7f3
aggre <hiroyuki.aggre@gmail.com>
2022-03-09 19:13:29 +0900

    add logging target

--- api/github.ts
+++ api/github.ts
@@ -6,5 +6,7 @@ export default function handler(
 ): VercelResponse {
 	// eslint-disable-next-line functional/no-expression-statement
 	console.log('req.body.action', req.body.action)
+	// eslint-disable-next-line functional/no-expression-statement
+	console.log('req.body.repository', req.body.repository)
 	return res.status(200).json(req.body)
 }
