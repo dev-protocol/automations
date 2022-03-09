85db04979b776702d5dad7623c4b72881b4bfa14
aggre <hiroyuki.aggre@gmail.com>
2022-03-09 19:02:41 +0900

    it worked

---
+++ api/github.ts
@@ -0,0 +1,10 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function handler(
+	req: VercelRequest,
+	res: VercelResponse
+): VercelResponse {
+	// eslint-disable-next-line functional/no-expression-statement
+	console.log({ req, res })
+	return res.status(200).json(req.body)
+}
